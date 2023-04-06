9fd650893be041f382ec12828fee79da486018b6
That Blackwolf <107058543+imnagarajan@users.noreply.github.com>
2023-04-06 14:47:33 +0530

    Update Program.cs

--- src/PluginTemplate/Program.cs
+++ src/PluginTemplate/Program.cs
@@ -1,123 +1,146 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
-using TShockAPI.Hooks;
 
-namespace BotPlugin
+namespace AIBot
 {
     [ApiVersion(2, 1)]
-    public class BotPlugin : TerrariaPlugin
+    public class AIBotPlugin : TerrariaPlugin
     {
-        public override string Name => "BotPlugin";
+        private static AIChatBot _bot;
+
+        public override string Name => "AIBot Plugin";
         public override string Author => "Your Name";
-        public override string Description => "A bot helper for players.";
+        public override string Description => "An AI functional bot for your TShock server";
         public override Version Version => new Version(1, 0, 0);
 
-        public BotPlugin(Main game) : base(game)
+        public AIBotPlugin(Main game) : base(game)
         {
         }
 
         public override void Initialize()
         {
-            ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
-            TShockAPI.Commands.ChatCommands.Add(new Command("bot.help", BotHelp, "tshelp"));
+            _bot = new AIChatBot();
+            ServerApi.Hooks.ServerChat.Register(this, OnServerChat);
         }
 
-        private void OnJoin(JoinEventArgs e)
+        private void OnServerChat(ServerChatEventArgs args)
         {
-            TShock.Utils.Broadcast($"{_config.WelcomeMessage} {e.Who}!");
-        }
-        
-        public class BotConfig
-        {
-            public string BotName { get; set; } = "Bot";
-            public string BotPrefix { get; set; } = "!";
-            public string BotCommandChar { get; set; } = "/";
-            public string BotCommandCharAlias { get; set; } = "!";
-            public string BotCommandCharAlias2 { get; set; } = ".";
-            public bool BotAnnounceConnections { get; set; } = true;
-            public string BotAnnouncementFormat { get; set; } = "[{0}] {1} has joined the server.";
-            public bool BotAnnounceDisconnections { get; set; } = true;
-            public string BotDisconnectionFormat { get; set; } = "[{0}] {1} has left the server.";
-            public bool BotLogChat { get; set; } = true;
-            public string BotLogChatFormat { get; set; } = "[{0}] [{1}] {2}: {3}";
-            public bool BotLogCommands { get; set; } = true;
-            public string BotLogCommandsFormat { get; set; } = "[{0}] {1}: {2}";
-            public bool BotLogWarnings { get; set; } = true;
-            public string BotLogWarningsFormat { get; set; } = "[{0}] WARNING: {1}";
-            public string BotCommandPrefix { get; set; } = "!";
-            public string BotCommandSuffix { get; set; } = "";
-            public bool BotAutoSave { get; set; } = true;
-            public int BotAutoSaveInterval { get; set; } = 600;
-            public bool BotAutoRestart { get; set; } = false;
-            public int BotAutoRestartInterval { get; set; } = 3600;
-        }
-
-
-        private void BotHelp(CommandArgs args)
-        {
-            // List of available bot commands
-            var message = "Available bot commands: !tptime, !tpme, !tpall, !healme, !healall";
-            args.Player.SendMessage(message, Color.Yellow);
-        }
-
-        private void TeleportPlayer(CommandArgs args, int targetIndex)
-        {
-            var target = TShock.Players[targetIndex];
-            if (target == null || !target.Active)
+            if (!args.Handled && args.Text.StartsWith(TShockAPI.TShock.Config.CommandSpecifier))
             {
-                args.Player.SendErrorMessage("Invalid player.");
-                return;
-            }
+                var text = args.Text.Substring(1);
+                var parts = text.Split(' ');
 
-            var player = args.Player;
-            var x = target.TileX * 16 + 8;
-            var y = target.TileY * 16;
-            player.Teleport(x, y);
-            player.SendSuccessMessage($"Teleported to {target.Name}.");
-        }
-        
-        
-        private void TeleportAll(CommandArgs args)
-        {
-            var player = args.Player;
-            foreach (var tsPlayer in TShock.Players)
-            {
-                if (tsPlayer != null && tsPlayer.Active && tsPlayer != player)
+                if (parts.Length == 1 && parts[0] == "hello")
                 {
-                    TeleportPlayer(args, tsPlayer.Index);
+                    TShock.Utils.Broadcast($"{args.Who} says hello to {_bot.Name}");
+                    args.Handled = true;
+                }
+                else
+                {
+                    var reply = _bot.GenerateReply(text, args.Who);
+                    if (!string.IsNullOrEmpty(reply))
+                    {
+                        TShock.Utils.Broadcast($"{_bot.Name}: {reply}");
+                        args.Handled = true;
+                    }
                 }
             }
-            player.SendSuccessMessage("Teleported all players.");
         }
 
-        private void HealPlayer(CommandArgs args, int targetIndex)
+        protected override void Dispose(bool disposing)
         {
-            var target = TShock.Players[targetIndex];
-            if (target == null || !target.Active)
+            if (disposing)
             {
-                args.Player.SendErrorMessage("Invalid player.");
-                return;
+                ServerApi.Hooks.ServerChat.Deregister(this, OnServerChat);
             }
 
-            target.Heal();
-            target.SendSuccessMessage($"You were healed by {args.Player.Name}.");
-            args.Player.SendSuccessMessage($"Healed {target.Name}.");
+            base.Dispose(disposing);
+        }
+    }
+
+    public class AIChatBot
+    {
+        private readonly string[] _greetings =
+        {
+            "Hi there!",
+            "Hello!",
+            "Greetings!",
+            "Howdy!",
+            "Hey!"
+        };
+
+        private readonly string[] _goodbyes =
+        {
+            "Goodbye!",
+            "See you later!",
+            "Farewell!",
+            "Take care!",
+            "Until next time!"
+        };
+
+        private readonly string[] _thanks =
+        {
+            "You're welcome!",
+            "No problem!",
+            "Happy to help!",
+            "Anytime!",
+            "Glad to be of service!"
+        };
+
+        private readonly string[] _confused =
+        {
+            "I'm sorry, I don't understand.",
+            "Could you please rephrase that?",
+            "I'm not sure what you mean.",
+            "Can you please clarify?",
+            "I don't know what to say to that."
+        };
+
+        private readonly Random _random = new Random();
+        public string Name { get; set; }
+
+        public AIChatBot()
+        {
+            Name = "AIBot";
         }
 
-        private void HealAll(CommandArgs args)
+        public string GenerateReply(string message, int playerId)
         {
-            var player = args.Player;
-            foreach (var tsPlayer in TShock.Players)
+            if (message.EndsWith("?"))
             {
-                if (tsPlayer != null && tsPlayer.Active && tsPlayer != player)
-                {
-                    HealPlayer(args, tsPlayer.Index);
-                }
+                return $"{_random.Choose(_confused)}";
             }
-            player.SendSuccessMessage("Healed all players.");
+            else if (message.Contains("hello"))
+            {
+                return $"{_random.Choose(_greetings)}";
+            }
+            else if (message.Contains("bye"))
+            {
+                return $"{_random.Choose(_goodbyes)}";
+            }
+            else if (message.Contains("thank"))
+            {
+                return $"{_random.Choose(_thanks)}";
+            }
+            else
+            {
+                return string.Empty;
+            }
         }
+    }
+
+ public static class Extensions
+{
+    public static T Choose<T>(this Random random, IEnumerable<T> source)
+    {
+        var count = source.Count();
+        var index = random.Next(count);
+        return source.ElementAt(index);
     }
 }
 
+    
