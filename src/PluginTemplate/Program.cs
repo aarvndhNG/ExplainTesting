fb2c7422d40fb1957c22d6b0eba90b0265d8a54a
That Blackwolf <107058543+imnagarajan@users.noreply.github.com>
2023-04-07 21:48:25 +0530

    Update Program.cs

--- src/PluginTemplate/Program.cs
+++ src/PluginTemplate/Program.cs
@@ -1,84 +1,117 @@
-﻿using System;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
 
-namespace RandomEnemyPlugin
+namespace CustomMessagesPlugin
 {
     [ApiVersion(2, 1)]
-    public class RandomEnemyPlugin : TerrariaPlugin
+    public class CustomMessagesPlugin : TerrariaPlugin
     {
         private Config _config;
+        private string _configPath = Path.Combine(TShock.SavePath, "CustomMessagesPlugin.json");
 
-        public override string Name => "RandomEnemyPlugin";
+        public override string Name => "CustomMessagesPlugin";
         public override string Author => "YourName";
-        public override string Description => "Spawns a random enemy.";
+        public override string Description => "Allows custom chat and join/leave messages.";
         public override Version Version => new Version(1, 0, 0);
 
-        public RandomEnemyPlugin(Main game) : base(game)
+        public CustomMessagesPlugin(Main game) : base(game)
         {
         }
 
         public override void Initialize()
         {
-            Commands.ChatCommands.Add(new Command("spawnenemy.spawn", SpawnRandomEnemy, "spawnenemy"));
+            ServerApi.Hooks.ServerChat.Register(this, OnChat);
+            ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
+            ServerApi.Hooks.ServerLeave.Register(this, OnLeave);
+
             LoadConfig();
+        }
+
+        public override void DeInitialize()
+        {
+            ServerApi.Hooks.ServerChat.Deregister(this, OnChat);
+            ServerApi.Hooks.ServerJoin.Deregister(this, OnJoin);
+            ServerApi.Hooks.ServerLeave.Deregister(this, OnLeave);
         }
 
         private void LoadConfig()
         {
-            _config = Config.Read();
+            try
+            {
+                if (!File.Exists(_configPath))
+                {
+                    _config = new Config();
+                    SaveConfig();
+                }
+                else
+                {
+                    using (var reader = new StreamReader(_configPath))
+                    {
+                        var json = reader.ReadToEnd();
+                        _config = JsonConvert.DeserializeObject<Config>(json);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                TShock.Log.Error(ex.ToString());
+            }
         }
 
         private void SaveConfig()
         {
-            _config.Write();
-        }
-
-        private void SpawnRandomEnemy(CommandArgs args)
-        {
-            int playerId = args.Player.Index;
-            TSPlayer player = TShock.Players[playerId];
-            if (player == null)
+            try
             {
-                args.Player.SendErrorMessage("You are not logged in.");
-                return;
+                using (var writer = new StreamWriter(_configPath))
+                {
+                    var json = JsonConvert.SerializeObject(_config, Formatting.Indented);
+                    writer.Write(json);
+                }
             }
-
-            var enemyTypes = _config.EnemyTypes;
-            if (enemyTypes.Count == 0)
+            catch (Exception ex)
             {
-                args.Player.SendErrorMessage("There are no enemy types configured.");
-                return;
+                TShock.Log.Error(ex.ToString());
             }
-
-            var random = new Random();
-            var randomIndex = random.Next(0, enemyTypes.Count);
-            var npcType = enemyTypes[randomIndex];
-
-            var spawnPosition = player.TPlayer.position;
-            spawnPosition.X += Main.rand.Next(-200, 200);
-            spawnPosition.Y -= 300;
-
-            NPC.NewNPC((int)spawnPosition.X, (int)spawnPosition.Y, npcType);
-            TShock.Utils.Broadcast($"A {Lang.GetNPCName(npcType)} has spawned near {player.Name}!", new Color(_config.SpawnAnnouncementColor));
-        }
-    }
-
-    internal class Config
-    {
-        public List<int> EnemyTypes { get; set; } = new List<int>();
-        public string SpawnAnnouncementColor { get; set; } = "yellow";
-
-        public static Config Read()
-        {
-            return new Config();
         }
 
-        public void Write()
+        private void OnChat(ServerChatEventArgs args)
         {
+            if (args.Handled)
+                return;
+
+            var message = args.Text;
+
+            if (_config.ChatMessages.TryGetValue(message, out string response))
+            {
+                TShock.Utils.Broadcast(response, _config.ChatMessageColor);
+                args.Handled = true;
+            }
+        }
+
+        private void OnJoin(JoinEventArgs args)
+        {
+            var message = _config.JoinMessage.Replace("{player}", args.Who.ToString());
+            TShock.Utils.Broadcast(message, _config.JoinLeaveMessageColor);
+        }
+
+        private void OnLeave(LeaveEventArgs args)
+        {
+            var message = _config.LeaveMessage.Replace("{player}", args.Who.ToString());
+            TShock.Utils.Broadcast(message, _config.JoinLeaveMessageColor);
+        }
+
+        private class Config
+        {
+            public Dictionary<string, string> ChatMessages { get; set; } = new Dictionary<string, string>();
+            public string ChatMessageColor { get; set; } = "yellow";
+            public string JoinMessage { get; set; } = "{player} joined the server.";
+            public string LeaveMessage { get; set; } = "{player} left the server.";
+            public string JoinLeaveMessageColor { get; set; } = "white";
         }
     }
 }
