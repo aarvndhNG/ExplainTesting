70703e17a3a6fd74d5d73e2111cedbf8ee0fd8c6
That Blackwolf <107058543+imnagarajan@users.noreply.github.com>
2023-04-08 11:36:53 +0530

    Update Program.cs

--- src/PluginTemplate/Program.cs
+++ src/PluginTemplate/Program.cs
@@ -1,43 +1,45 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using Newtonsoft.Json;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
 
-namespace CustomMessagesPlugin
+namespace WinterMinigame
 {
     [ApiVersion(2, 1)]
-    public class CustomMessagesPlugin : TerrariaPlugin
+    public class WinterMinigame : TerrariaPlugin
     {
         private Config _config;
+        private List<TSPlayer> _players;
+        private DateTime _gameStartTime;
+        private DateTime _gameEndTime;
 
-        public override string Name => "CustomMessagesPlugin";
+        public override string Name => "WinterMinigame";
         public override string Author => "YourName";
-        public override string Description => "Custom join, leave, and chat messages.";
+        public override string Description => "Winter-themed minigame.";
         public override Version Version => new Version(1, 0, 0);
 
-        public CustomMessagesPlugin(Main game) : base(game)
+        public WinterMinigame(Main game) : base(game)
         {
+            Order = 2;
         }
 
         public override void Initialize()
         {
-            ServerApi.Hooks.ServerJoin.Register(this, OnServerJoin);
-            ServerApi.Hooks.ServerLeave.Register(this, OnServerLeave);
-            TShockAPI.Hooks.GameHooks.PostGetData.Register(this, OnPostGetData);
+            _players = new List<TSPlayer>();
+
+            Commands.ChatCommands.Add(new Command("winterminigame.start", StartGame, "startgame"));
+            Commands.ChatCommands.Add(new Command("winterminigame.stop", StopGame, "stopgame"));
+            Commands.ChatCommands.Add(new Command("winterminigame.status", GetGameStatus, "gamestatus"));
+
             LoadConfig();
+            StartGame(null, new CommandArgs(TShock.Players[0], "", new string[] { }));
         }
 
         public override void DeInitialize()
         {
-            base.DeInitialize();
-            
-            ServerApi.Hooks.ServerJoin.Deregister(this, OnServerJoin);
-            ServerApi.Hooks.ServerLeave.Deregister(this, OnServerLeave);
-            TShockAPI.Hooks.GameHooks.PostGetData.Deregister(this, OnPostGetData);
-            SaveConfig();
+            StopGame(null, new CommandArgs(TShock.Players[0], "", new string[] { }));
+            _players = null;
         }
 
         private void LoadConfig()
@@ -50,54 +52,123 @@ namespace CustomMessagesPlugin
             _config.Write();
         }
 
-        private void OnServerJoin(JoinEventArgs args)
+        private void StartGame(CommandArgs args)
         {
-            TShock.Utils.Broadcast(string.Format(_config.JoinMessage, args.Who, TShock.Players[args.Who].Name), _config.JoinColor);
-        }
-
-        private void OnServerLeave(LeaveEventArgs args)
-        {
-            TShock.Utils.Broadcast(string.Format(_config.LeaveMessage, args.Who, TShock.Players[args.Who].Name), _config.LeaveColor);
-        }
-
-        private void OnPostGetData(GetDataEventArgs args)
-        {
-            if (args.Handled)
-                return;
-
-            if (args.MsgID == PacketTypes.ChatText && args.number1 >= 0 && args.number1 < 255)
+            if (_gameStartTime != DateTime.MinValue)
             {
-                var player = TShock.Players[args.number1];
-                if (player == null)
-                    return;
+                args.Player.SendErrorMessage("A game is already in progress.");
+                return;
+            }
 
-                var message = args.readBuffer.ReadString().Trim();
-                if (string.IsNullOrWhiteSpace(message))
-                    return;
+            _gameStartTime = DateTime.Now;
+            _gameEndTime = DateTime.Now.AddMinutes(_config.GameDuration);
+            _players.Clear();
 
-                TShock.Utils.Broadcast(string.Format(_config.ChatMessage, player.Name, message), _config.ChatColor);
+            foreach (var player in TShock.Players)
+            {
+                if (player != null && player.Active)
+                {
+                    _players.Add(player);
+                    player.Teleport(_config.StartingPosition.X, _config.StartingPosition.Y);
+                }
+            }
+
+            TShock.Utils.Broadcast($"A winter-themed minigame has started! Type /join to join. The game will end in {_config.GameDuration} minutes.", Color.Yellow);
+        }
+
+        private void StopGame(CommandArgs args)
+        {
+            if (_gameStartTime == DateTime.MinValue)
+            {
+                args.Player.SendErrorMessage("There is no game in progress.");
+                return;
+            }
+
+            _gameStartTime = DateTime.MinValue;
+            _gameEndTime = DateTime.MinValue;
+            _players.Clear();
+
+            TShock.Utils.Broadcast("The winter-themed minigame has ended.", Color.Yellow);
+        }
+
+        private void GetGameStatus(CommandArgs args)
+        {
+            if (_gameStartTime == DateTime.MinValue)
+            {
+                args.Player.SendErrorMessage("There is no game in progress.");
+                return;
+            }
+
+            var timeRemaining = (_gameEndTime - DateTime.Now).ToString(@"mm\:ss");
+            var message = $"There is a winter-themed minigame in progress. {_players.Count} players have joined. Time remaining: {timeRemaining}";
+            args.Player.SendInfoMessage(message);
+        }
+
+        private void OnJoin(JoinEventArgs args)
+        {
+            if (_gameStartTime != DateTime.MinValue)
+            {
+                _players.Add(args.Player);
+                args.Player.Teleport(_config.StartingPosition.X, _config.StartingPosition.Y);
+                TShock.Utils.Broadcast($"{args.Player.Name} has joined the game.", Color.Yellow);
             }
         }
-    }
 
-    internal class Config
-    {
-        public string JoinMessage { get; set; } = "{1} ({0}) has joined the server.";
-        public string JoinColor { get; set; } = "yellow";
-
-        public string LeaveMessage { get; set; } = "{1} ({0}) has left the server.";
-        public string LeaveColor { get; set; } = "yellow";
-
-        public string ChatMessage { get; set; } = "{0}: {1}";
-        public string ChatColor { get; set; } = "white";
-
-        public static Config Read()
+        private void OnLeave(LeaveEventArgs args)
         {
-            return new Config();
+          if (_gameStartTime != DateTime.MinValue)
+        {
+        _players.Remove(args.Player);
+        TShock.Utils.Broadcast($"{args.Player.Name} has left the game.", Color.Yellow);
         }
-
-        public void Write()
+        }
+        public override void OnInitialize()
         {
+        ServerApi.Hooks.GameInitialize.Register(this, OnGameInitialize);
+        ServerApi.Hooks.ServerLeave.Register(this, OnServerLeave);
+        ServerApi.Hooks.NetGreetPlayer.Register(this, OnGreetPlayer);
+        }
+       
+       private void OnGameInitialize(EventArgs args)
+        {
+        Commands.ChatCommands.Add(new Command("winterminigame.join", JoinGame, "join"));
+        }
+       
+       private void OnServerLeave(LeaveEventArgs args)
+        {
+          if (_gameStartTime != DateTime.MinValue)
+          {
+            _players.Remove(args.Player);
+          }
+        }
+       
+       private void OnGreetPlayer(GreetPlayerEventArgs args)
+       {
+           if (_gameStartTime != DateTime.MinValue)
+          {
+              args.Handled = true;
+              args.Player.SendInfoMessage("A winter-themed minigame is in progress! Type /join to join.");
+          }
+        }
+        
+        private void JoinGame(CommandArgs args)
+        {
+            if (_gameStartTime == DateTime.MinValue)
+           {
+                args.Player.SendErrorMessage("There is no game in progress.");
+                return;
+           }
+           
+           if (_players.Contains(args.Player))
+          {
+               args.Player.SendErrorMessage("You have already joined the game.");
+               return;
+           }
+           
+           _players.Add(args.Player);
+        args.Player.Teleport(_config.StartingPosition.X, _config.StartingPosition.Y);
+        TShock.Utils.Broadcast($"{args.Player.Name} has joined the game.", Color.Yellow);
         }
     }
 }
+
