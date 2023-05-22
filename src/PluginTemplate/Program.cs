2cc70581a612e3587ea73bc8458b91f064fc5ddd
That Blackwolf <107058543+aarvndhNG@users.noreply.github.com>
2023-05-22 15:02:30 +0530

    Update Program.cs

--- src/PluginTemplate/Program.cs
+++ src/PluginTemplate/Program.cs
@@ -1,187 +1,195 @@
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Timers;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
+using TShockAPI.Hooks;
 
-namespace HelperBot
+namespace SocialFeaturesPlugin
 {
     [ApiVersion(2, 1)]
-    public class HelperBot : TerrariaPlugin
+    public class SocialFeaturesPlugin : TerrariaPlugin
     {
-        #region Plugin Info
-        public override string Name => "HelperBot";
-        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
-        public override string Author => "Ryozuki";
-        public override string Description => "A bot with multiple utilities, such as stats gathering and a Q&A system.";
-        #endregion
+        private Dictionary<string, UserProfile> playerProfiles;
+        private Dictionary<string, List<string>> friendLists;
+        private Dictionary<string, int> playerScores;
+        private List<string> globalChatChannels;
 
-        public ConfigFile Config { get; set; } = new ConfigFile();
-        public Regex ReminderRegex { get; set; } = new Regex(@"remind me to ([\w+ ]+) in (\d+) (minutes|mins|hours|seconds|secs|days)");
+        public override string Name => "SocialFeaturesPlugin";
+        public override Version Version => new Version(1, 0, 0);
+        public override string Author => "Your Name";
+        public override string Description => "Adds social features to your TShock server.";
 
-        public HelperBot(Main game) : base(game)
-        {
-        }
-
-        private void LoadConfig()
-        {
-            string path = Path.Combine(TShock.SavePath, "HelperBot.json");
-            Config = ConfigFile.Read(path);
-        }
+        public SocialFeaturesPlugin(Main game) : base(game) { }
 
         public override void Initialize()
         {
-            LoadConfig();
-            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
-            ServerApi.Hooks.ServerChat.Register(this, OnChat, -666);
+            playerProfiles = new Dictionary<string, UserProfile>();
+            friendLists = new Dictionary<string, List<string>>();
+            playerScores = new Dictionary<string, int>();
+            globalChatChannels = new List<string>();
+
+            // Register chat commands
+            Commands.ChatCommands.Add(new Command("profile", ProfileCommand, "profile"));
+            Commands.ChatCommands.Add(new Command("leaderboard", LeaderboardCommand, "leaderboard"));
+            Commands.ChatCommands.Add(new Command("friend", FriendCommand, "friend"));
+            Commands.ChatCommands.Add(new Command("globalchat", GlobalChatCommand, "globalchat"));
+
+            // Register event handlers
+            PlayerHooks.PlayerPostLogin += OnPlayerPostLogin;
+            ServerApi.Hooks.NetGreetPlayer.Register(this, OnNetGreetPlayer);
+        }
+
+        private void OnPlayerPostLogin(PlayerPostLoginEventArgs args)
+        {
+            string playerName = args.Player.Name;
+
+            // Initialize player profile
+            if (!playerProfiles.ContainsKey(playerName))
+                playerProfiles.Add(playerName, new UserProfile(playerName));
+
+            // Initialize friend list
+            if (!friendLists.ContainsKey(playerName))
+                friendLists.Add(playerName, new List<string>());
+
+            // Initialize player score
+            if (!playerScores.ContainsKey(playerName))
+                playerScores.Add(playerName, 0);
+        }
+
+        private void OnNetGreetPlayer(GreetPlayerEventArgs args)
+        {
+            TSPlayer player = TShock.Players[args.Who];
+
+            // Send welcome message and instructions to the player
+            player.SendInfoMessage("Welcome to the server!");
+            player.SendInfoMessage("Use /profile to view your profile.");
+            player.SendInfoMessage("Use /leaderboard to view the server leaderboard.");
+            player.SendInfoMessage("Use /friend to manage your friends list.");
+            player.SendInfoMessage("Use /globalchat to switch to global chat channel.");
+        }
+
+        private void ProfileCommand(CommandArgs args)
+        {
+            string playerName = args.Player.Name;
+
+            // Retrieve and display player profile information
+            UserProfile profile = playerProfiles[playerName];
+            args.Player.SendInfoMessage($"Profile Information - Name: {profile.Name}, Level: {profile.Level}, Exp: {profile.Exp}");
+        }
+
+        private void LeaderboardCommand(CommandArgs args)
+        {
+            // Retrieve and display the server leaderboard
+            List<string> leaderboard = GetLeaderboard();
+            args.Player.SendInfoMessage("Server Leaderboard:");
+            for (int i = 0; i < leaderboard.Count; i++)
+            {
+                string entry = $"{i + 1}. {leaderboard[i]}";
+                args.Player.SendInfoMessage(entry);
+            }
+        }
+
+        private void FriendCommand(CommandArgs args)
+        {
+            string playerName = args.Player.Name;
+            string friendName = args.Parameters.Count > 0 ? args.Parameters[0] : "";
+
+            if (string.IsNullOrWhiteSpace(friendName))
+            {
+                // Display friend list
+                List<string> friends = friendLists[playerName];
+                if (friends.Count == 0)
+                {
+                    args.Player.SendInfoMessage("You have no friends.");
+                }
+                else
+                {
+                    args.Player.SendInfoMessage("Your Friends:");
+                    foreach (string friend in friends)
+                        args.Player.SendInfoMessage(friend);
+                }
+            }
+            else
+            {
+                // Add or remove friend
+                if (friendLists[playerName].Contains(friendName))
+                {
+                    friendLists[playerName].Remove(friendName);
+                    args.Player.SendSuccessMessage($"Removed {friendName} from your friends list.");
+                }
+                else
+                {
+                    friendLists[playerName].Add(friendName);
+                    args.Player.SendSuccessMessage($"Added {friendName} to your friends list.");
+                }
+            }
+        }
+
+        private void GlobalChatCommand(CommandArgs args)
+        {
+            string playerName = args.Player.Name;
+
+            // Switch to global chat channel
+            if (globalChatChannels.Contains(playerName))
+            {
+                globalChatChannels.Remove(playerName);
+                args.Player.SendSuccessMessage("You have left the global chat channel.");
+            }
+            else
+            {
+                globalChatChannels.Add(playerName);
+                args.Player.SendSuccessMessage("You have joined the global chat channel.");
+            }
+        }
+
+        private List<string> GetLeaderboard()
+        {
+            // Retrieve and sort player scores
+            List<string> leaderboard = new List<string>();
+            foreach (KeyValuePair<string, int> entry in playerScores)
+                leaderboard.Add($"{entry.Key}: {entry.Value}");
+
+            leaderboard.Sort((a, b) =>
+            {
+                string[] partsA = a.Split(':');
+                string[] partsB = b.Split(':');
+                int scoreA = int.Parse(partsA[1].Trim());
+                int scoreB = int.Parse(partsB[1].Trim());
+                return scoreB.CompareTo(scoreA);
+            });
+
+            return leaderboard;
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+                // Dispose of resources, clean up connections, etc.
+                playerProfiles.Clear();
+                friendLists.Clear();
+                playerScores.Clear();
+                globalChatChannels.Clear();
+
+                PlayerHooks.PlayerPostLogin -= OnPlayerPostLogin;
+                ServerApi.Hooks.NetGreetPlayer.Deregister(this, OnNetGreetPlayer);
             }
             base.Dispose(disposing);
         }
+    }
 
-        #region Hooks
-        private void OnInitialize(EventArgs args)
+    public class UserProfile
+    {
+        public string Name { get; }
+        public int Level { get; set; }
+        public int Exp { get; set; }
+
+        public UserProfile(string name)
         {
-            Commands.ChatCommands.Add(new Command("helperbot.cmds", CommandHandler, Config.BaseCommandName)
-            {
-                HelpText = $"Usage: {TShock.Config.CommandSpecifier}{Config.BaseCommandName} <command>"
-            });
+            Name = name;
+            Level = 1;
+            Exp = 0;
         }
-
-        private void OnChat(ServerChatEventArgs e)
-        {
-            string msg = e.Text;
-
-            var ply = TShock.Players[e.Who];
-
-            if (ply == null)
-                return;
-
-            msg = msg.ToLower();
-
-            if (Config.QuestionsAndAnswers.Any(x => Regex.Match(msg.ToLower(), x.Question.ToLower()).Success))
-            {
-                var answer = Config.QuestionsAndAnswers.Find(x => Regex.Match(msg.ToLower(), x.Question.ToLower()).Success)?.Answer;
-
-                TShockAPI.Utils.Broadcast($"{Config.BotName}: {answer}", (byte)Config.BotColor[0], (byte)Config.BotColor[1], (byte)Config.BotColor[2]);
-            }
-            else if (msg.Contains("how much is"))
-            {
-                int index = msg.LastIndexOf('s');
-                string op = msg.Substring(index + 1);
-                op = op.Replace("?", "");
-
-                try
-                {
-                    double result = Convert.ToDouble(new DataTable().Compute(op, null));
-                    TShockAPI.Utils.Broadcast($"{Config.BotName}: The result is: {result}", (byte)Config.BotColor[0], (byte)Config.BotColor[1], (byte)Config.BotColor[2]);
-                }
-                catch (System.Data.EvaluateException)
-                {
-                    // invalid operation
-                }
-                catch (System.Data.SyntaxErrorException)
-                {
-                    // invalid operation
-                }
-            }
-            else if (ReminderRegex.IsMatch(msg) && ply.HasPermission("helperbot.remind"))
-            {
-                Match m = ReminderRegex.Match(msg);
-
-                string what = m.Groups[1].Value;
-                float time = float.Parse(m.Groups[2].Value);
-                string time_unit = m.Groups[3].Value;
-
-                if (time_unit == "mins")
-                    time_unit = "minutes";
-                if (time_unit == "secs")
-                    time_unit = "seconds";
-
-                TShockAPI.Utils.Broadcast($"{Config.BotName}: {ply.Name} I'll remind you to {what} in {time} {time_unit}", (byte)Config.BotColor[0], (byte)Config.BotColor[1], (byte)Config.BotColor[2]);
-
-                if (time_unit == "days")
-                    time *= 24 * 3600 * 1000;
-                if (time_unit == "hours")
-                    time *= 3600 * 1000;
-                else if (time_unit == "minutes")
-                    time *= 60 * 1000;
-                else if (time_unit == "seconds")
-                    time *= 1000;
-
-                Timer timer = new Timer(time);
-                timer.Elapsed += (sender, ee) => TShockAPI.Utils.Broadcast($"{Config.BotName}: {ply.Name} remember to {what}", (byte)Config.BotColor[0], (byte)Config.BotColor[1], (byte)Config.BotColor[2]);
-                timer.AutoReset = false;
-                timer.Start();
-            }
-        }
-        #endregion
-
-        #region Commands
-        private void CommandHandler(CommandArgs e)
-        {
-            if (!Config.EnableBotCommands)
-                return;
-
-            var ply = e.Player;
-            var cmd = "";
-
-            if (e.Parameters.Count != 0)
-            {
-                cmd = e.Parameters[0];
-            }
-
-            var args = e.Parameters.Skip(1).ToArray();
-
-            switch (cmd.ToLower())
-            {
-                case "register":
-                    if (args.Length < 1)
-                    {
-                        ply.SendErrorMessage("Usage: {0}register <password>", TShock.Config.CommandSpecifier);
-                        return;
-                    }
-
-                    if (ply.User != null)
-                    {
-                        ply.SendErrorMessage("You are already registered.");
-                        return;
-                    }
-
-                    var registerPassword = args[0];
-
-                    if (ply.Register(registerPassword))
-                    {
-                        ply.SendSuccessMessage("You have successfully registered.");
-                    }
-                    else
-                    {
-                        ply.SendErrorMessage("Registration failed.");
-                    }
-                    break;
-
-                case "spawn":
-                    ply.Teleport(TShock.Players.FirstOrDefault(p => p?.IsLoggedIn == true)?.TileX ?? 0 * 16, TShock.Players.FirstOrDefault(p => p?.IsLoggedIn == true)?.TileY ?? 0 * 16);
-                    break;
-
-                default:
-                    ply.SendErrorMessage("Invalid command. Type {0}help {1} for a list of available commands.", TShock.Config.CommandSpecifier, Config.BaseCommandName);
-                    break;
-            }
-        }
-        #endregion
     }
 }
