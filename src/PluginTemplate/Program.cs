97b96e3c131e0c23f06ffe9f431e1ae7d36ad528
That Blackwolf <107058543+aarvndhNG@users.noreply.github.com>
2023-05-23 12:27:27 +0530

    Update Program.cs

--- src/PluginTemplate/Program.cs
+++ src/PluginTemplate/Program.cs
@@ -1,213 +1,123 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using Newtonsoft.Json;
+using System.Linq;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
-using Microsoft.Xna.Framework;
 
-namespace CustomChatChannels
+namespace Paintball
 {
     [ApiVersion(2, 1)]
-    public class CustomChatChannelsPlugin : TerrariaPlugin
+    public class PaintballPlugin : TerrariaPlugin
     {
-        private Dictionary<string, string> playerChannels;
-        private Dictionary<string, Color> channelColors;
-        private string configFile = Path.Combine(TShock.SavePath, "CustomChatChannelsConfig.json");
-
-        public override string Name => "CustomChatChannels";
+        public override string Name => "Paintball";
         public override Version Version => new Version(1, 0, 0);
         public override string Author => "YourName";
-        public override string Description => "Allows players to create and manage custom chat channels.";
+        public override string Description => "A fast-paced game where players shoot paintballs at each other in an arena, trying to eliminate opponents and be the last one standing.";
 
-        public CustomChatChannelsPlugin(Main game) : base(game)
+        private List<string> playersInGame;
+        private Dictionary<string, DateTime> lastShootTimes;
+
+        private int paintballItemId; // Configurable paintball item ID
+        private int paintballProjectileType; // Configurable paintball projectile type
+
+        public PaintballPlugin(Main game) : base(game)
         {
         }
 
         public override void Initialize()
         {
-            playerChannels = new Dictionary<string, string>();
-            channelColors = new Dictionary<string, Color>();
+            playersInGame = new List<string>();
+            lastShootTimes = new Dictionary<string, DateTime>();
 
-            TShockAPI.Commands.ChatCommands.Add(new Command("customchat.channel.create", CreateChannel, "createchannel"));
-            TShockAPI.Commands.ChatCommands.Add(new Command("customchat.channel.join", JoinChannel, "joinchannel"));
-            TShockAPI.Commands.ChatCommands.Add(new Command("customchat.channel.leave", LeaveChannel, "leavechannel"));
-            TShockAPI.Commands.ChatCommands.Add(new Command("customchat.channel.chat", ChatChannel, "chat", "c"));
-            TShockAPI.Commands.ChatCommands.Add(new Command("customchat.channel.list", ListChannels, "listchannels"));
-            TShockAPI.Commands.ChatCommands.Add(new Command("customchat.channel.color", SetChannelColor, "setchannelcolor"));
-            ServerApi.Hooks.ServerChat.Register(this, OnChat);
+            ServerApi.Hooks.NetGetData.Register(this, OnGetData);
+            TShockAPI.Commands.ChatCommands.Add(new Command("paintball", PaintballCommand, "paintball"));
 
-            LoadConfig();
+            // Load configuration values
+            paintballItemId = /* Set the item ID for the paintball item */;
+            paintballProjectileType = /* Set the projectile type for the paintball projectile */;
         }
 
-        private void OnChat(ServerChatEventArgs args)
+        public override void DeInitialize()
         {
-            var player = TShock.Players[args.Who];
-            if (player == null || !playerChannels.ContainsKey(player.Name))
-                return;
-
-            var channel = playerChannels[player.Name];
-            if (channel == null)
-                return;
-
-            var channelColor = channelColors.ContainsKey(channel) ? channelColors[channel] : Color.White;
-            var message = $"[{channel}] {player.Name}: {args.Text}";
-
-            TSPlayer.All.SendMessage(message, channelColor);
-
-            args.Handled = true;
+            playersInGame.Clear();
+            lastShootTimes.Clear();
         }
 
-        private void CreateChannel(CommandArgs args)
+        private void OnGetData(GetDataEventArgs args)
+        {
+            if (args.MsgID == PacketTypes.PlayerUpdate)
+            {
+                var playerIndex = args.Msg.whoAmI;
+                var player = TShock.Players[playerIndex];
+
+                if (player == null || !playersInGame.Contains(player.Name))
+                    return;
+
+                var velX = BitConverter.ToSingle(args.Msg.readBuffer, args.Index + 3);
+                var velY = BitConverter.ToSingle(args.Msg.readBuffer, args.Index + 7);
+
+                if (Math.Abs(velX) > 0.01f || Math.Abs(velY) > 0.01f)
+                {
+                    // Player is moving, check if they are trying to shoot a paintball
+                    if (DateTime.Now - lastShootTimes[player.Name] > TimeSpan.FromSeconds(0.5))
+                    {
+                        var paintballItem = new Item();
+                        paintballItem.SetDefaults(paintballItemId);
+                        paintballItem.stack = 1;
+
+                        var paintballSpeed = /* Set the speed for the paintball projectile */;
+
+                        var paintball = Terraria.Projectile.NewProjectile(player.position.X, player.position.Y, velX * paintballSpeed, velY * paintballSpeed, paintballProjectileType, paintballItem.damage, paintballItem.knockBack, playerIndex);
+                        NetMessage.SendData(27, -1, -1, null, paintball);
+                        lastShootTimes[player.Name] = DateTime.Now;
+                    }
+                }
+            }
+        }
+
+        private void PaintballCommand(CommandArgs args)
         {
             if (args.Parameters.Count < 1)
             {
-                args.Player.SendErrorMessage("Invalid syntax! Proper syntax: /createchannel <channelname>");
+                args.Player.SendErrorMessage("Invalid syntax! Proper syntax: /paintball <join|leave>");
                 return;
             }
 
-            var channelName = args.Parameters[0];
+            var subCommand = args.Parameters[0];
 
-            if (playerChannels.ContainsKey(channelName))
+            switch (subCommand.ToLower())
             {
-                args.Player.SendErrorMessage($"Channel '{channelName}' already exists.");
-                return;
-            }
+                case "join":
+                    if (!playersInGame.Contains(args.Player.Name))
+                    {
+                        playersInGame.Add(args.Player.Name);
+                        lastShootTimes[args.Player.Name] = DateTime.Now;
+                        args.Player.SendSuccessMessage("You joined the paintball game.");
+                    }
+                    else
+                    {
+                        args.Player.SendInfoMessage("You are already in the paintball game.");
+                    }
+                    break;
 
-            playerChannels[args.Player.Name] = channelName;
-            args.Player.SendSuccessMessage($"Created and joined channel '{channelName}'.");
-        }
+                case "leave":
+                    if (playersInGame.Contains(args.Player.Name))
+                    {
+                        playersInGame.Remove(args.Player.Name);
+                        lastShootTimes.Remove(args.Player.Name);
+                        args.Player.SendSuccessMessage("You left the paintball game.");
+                    }
+                    else
+                    {
+                        args.Player.SendInfoMessage("You are not currently in the paintball game.");
+                    }
+                    break;
 
-        private void JoinChannel(CommandArgs args)
-        {
-            if (args.Parameters.Count < 1)
-            {
-                args.Player.SendErrorMessage("Invalid syntax! Proper syntax: /joinchannel <channelname>");
-                return;
-            }
-
-            var channelName = args.Parameters[0];
-
-            if (!playerChannels.ContainsKey(channelName))
-            {
-                args.Player.SendErrorMessage($"Channel '{channelName}' does not exist.");
-                return;
-            }
-
-            playerChannels[args.Player.Name] = channelName;
-            args.Player.SendSuccessMessage($"Joined channel '{channelName}'.");
-        }
-
-        private void LeaveChannel(CommandArgs args)
-        {
-            if (!playerChannels.ContainsKey(args.Player.Name))
-            {
-                args.Player.SendErrorMessage("You are not in a channel.");
-                return;
-            }
-
-            var channel = playerChannels[args.Player.Name];
-            playerChannels.Remove(args.Player.Name);
-            args.Player.SendSuccessMessage($"Left channel '{channel}'.");
-        }
-
-        private void ChatChannel(CommandArgs args)
-        {
-            if (!playerChannels.ContainsKey(args.Player.Name))
-            {
-                args.Player.SendErrorMessage("You are not in a channel.");
-                return;
-            }
-
-            var channel = playerChannels[args.Player.Name];
-            var channelColor = channelColors.ContainsKey(channel) ? channelColors[channel] : Color.White;
-            var message = string.Join(" ", args.Parameters);
-
-            TSPlayer.All.SendMessage($"[{channel}] {args.Player.Name}: {message}", channelColor);
-        }
-
-        private void ListChannels(CommandArgs args)
-        {
-            var channelList = string.Join(", ", playerChannels.Values);
-            args.Player.SendInfoMessage($"Channels: {channelList}");
-        }
-
-        private void SetChannelColor(CommandArgs args)
-        {
-            if (args.Parameters.Count < 4)
-            {
-                args.Player.SendErrorMessage("Invalid syntax! Proper syntax: /setchannelcolor <channelname> <R> <G> <B>");
-                return;
-            }
-
-            var channelName = args.Parameters[0];
-            var rStr = args.Parameters[1];
-            var gStr = args.Parameters[2];
-            var bStr = args.Parameters[3];
-
-            if (!playerChannels.ContainsKey(channelName))
-            {
-                args.Player.SendErrorMessage($"Channel '{channelName}' does not exist.");
-                return;
-            }
-
-            if (!int.TryParse(rStr, out var r) || !int.TryParse(gStr, out var g) || !int.TryParse(bStr, out var b))
-            {
-                args.Player.SendErrorMessage("Invalid color values specified.");
-                return;
-            }
-
-            var color = new Color(r, g, b);
-            channelColors[channelName] = color;
-
-            args.Player.SendSuccessMessage($"Set color for channel '{channelName}' to ({r}, {g}, {b}).");
-        }
-
-        private void LoadConfig()
-        {
-            if (File.Exists(configFile))
-            {
-                try
-                {
-                    var json = File.ReadAllText(configFile);
-                    channelColors = JsonConvert.DeserializeObject<Dictionary<string, Color>>(json);
-                }
-                catch (Exception ex)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Error loading configuration:");
-                    Console.WriteLine(ex);
-                    Console.ResetColor();
-                }
-            }
-
-            if (channelColors == null)
-                channelColors = new Dictionary<string, Color>();
-        }
-
-        private void SaveConfig()
-        {
-            try
-            {
-                var json = JsonConvert.SerializeObject(channelColors, Formatting.Indented);
-                File.WriteAllText(configFile, json);
-            }
-            catch (Exception ex)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Error saving configuration:");
-                Console.WriteLine(ex);
-                Console.ResetColor();
+                default:
+                    args.Player.SendErrorMessage("Invalid subcommand! Available subcommands: join, leave");
+                    break;
             }
         }
-
-        public void DeInitialize()
-        {
-            playerChannels.Clear();
-            channelColors.Clear();
-            SaveConfig();
-        }
-
     }
 }
