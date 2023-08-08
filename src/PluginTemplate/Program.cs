1cbaee7bf210a91c413da5c239c99ede7205f95c
That Blackwolf <107058543+aarvndhNG@users.noreply.github.com>
2023-08-08 09:35:30 +0530

    Update Program.cs

--- src/PluginTemplate/Program.cs
+++ src/PluginTemplate/Program.cs
@@ -1,31 +1,31 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using Newtonsoft.Json;
+using System.Net.Http;
 using Terraria;
-using TerrariaApi.Server;
 using TShockAPI;
+using TShockAPI.Hooks;
+using TerrariaApi.Server;
+using Newtonsoft.Json.Linq;
 
-namespace BossRewards
+namespace ServerStatusPlugin
 {
     [ApiVersion(2, 1)]
-    public class BossRewards : TerrariaPlugin
+    public class ServerStatusPlugin : TerrariaPlugin
     {
-        public override string Name => "BossRewards";
-        public override string Author => "YourName";
-        public override string Description => "Provides configurable rewards for defeating bosses.";
+        private ServerStatusConfig _config;
+
+        public override string Author => "Your Name";
+        public override string Description => "Displays server status from your website.";
+        public override string Name => "ServerStatusPlugin";
         public override Version Version => new Version(1, 0, 0);
 
-        private Dictionary<int, BossReward> bossRewards;
-
-        public BossRewards(Main game) : base(game)
-        {
-            bossRewards = new Dictionary<int, BossReward>();
-        }
+        public ServerStatusPlugin(Main game) : base(game) { }
 
         public override void Initialize()
         {
-            ServerApi.Hooks.NpcKilled.Register(this, OnNpcKilled);
+            TShockAPI.Hooks.GeneralHooks.ReloadEvent += OnReload;
+            TShockAPI.Hooks.PlayerHooks.PlayerPostLogin += OnPlayerPostLogin;
+
             SetupConfig();
         }
 
@@ -33,85 +33,70 @@ namespace BossRewards
         {
             if (disposing)
             {
-                ServerApi.Hooks.NpcKilled.Deregister(this, OnNpcKilled);
+                TShockAPI.Hooks.GeneralHooks.ReloadEvent -= OnReload;
+                TShockAPI.Hooks.PlayerHooks.PlayerPostLogin -= OnPlayerPostLogin;
             }
             base.Dispose(disposing);
         }
 
+        private void OnReload(ReloadEventArgs args)
+        {
+            SetupConfig();
+        }
+
+        private void OnPlayerPostLogin(PlayerPostLoginEventArgs args)
+        {
+            CheckServerStatus(args.Player);
+        }
+
+        private async void CheckServerStatus(TSPlayer player)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    var response = await httpClient.GetStringAsync(_config.ApiUrl);
+                    var jsonResponse = JObject.Parse(response);
+                    var serverStatus = jsonResponse["status"].ToString();
+
+                    string statusMessage = serverStatus == "online"
+                        ? "Server Status: Online"
+                        : "Server Status: Offline";
+
+                    TShock.Utils.Broadcast(statusMessage, Color.Green);
+                }
+                catch (Exception ex)
+                {
+                    TShock.Log.Error(ex.ToString());
+                }
+            }
+        }
+
         private void SetupConfig()
         {
-            string configPath = Path.Combine(TShock.SavePath, "BossRewardsConfig.json");
+            string configPath = Path.Combine(TShock.SavePath, "ServerStatusConfig.json");
 
-            if (File.Exists(configPath))
+            if (!File.Exists(configPath))
             {
-                bossRewards = JsonConvert.DeserializeObject<Dictionary<int, BossReward>>(File.ReadAllText(configPath));
+                _config = new ServerStatusConfig();
+                File.WriteAllText(configPath, JsonConvert.SerializeObject(_config, Formatting.Indented));
             }
             else
             {
-                bossRewards = CreateDefaultConfig();
-                File.WriteAllText(configPath, JsonConvert.SerializeObject(bossRewards, Formatting.Indented));
+                _config = JsonConvert.DeserializeObject<ServerStatusConfig>(File.ReadAllText(configPath));
+            }
+
+            if (string.IsNullOrWhiteSpace(_config.GeneratedUrl))
+            {
+                _config.GeneratedUrl = GenerateServerUrl();
+                File.WriteAllText(configPath, JsonConvert.SerializeObject(_config, Formatting.Indented));
             }
         }
 
-        private Dictionary<int, BossReward> CreateDefaultConfig()
+        private string GenerateServerUrl()
         {
-            Dictionary<int, BossReward> defaultConfig = new Dictionary<int, BossReward>();
-
-            // Example rewards for defeating bosses
-            defaultConfig[NPCID.EyeofCthulhu] = new BossReward
-            {
-                ItemID = ItemID.GoldCoin,
-                Amount = 10,
-                Message = "You received 10 gold coins for defeating the Eye of Cthulhu!"
-            };
-
-            defaultConfig[NPCID.EaterofWorldsHead] = new BossReward
-            {
-                ItemID = ItemID.IronskinPotion,
-                Amount = 5,
-                Message = "You received 5 Ironskin Potions for defeating the Eater of Worlds!"
-            };
-
-            // Add more entries for other bosses and their respective rewards
-            defaultConfig[NPCID.SkeletronHead] = new BossReward
-            {
-                ItemID = ItemID.ManaCrystal,
-                Amount = 1,
-                Message = "You received 1 Mana Crystal for defeating Skeletron!"
-            };
-
-            defaultConfig[NPCID.WallofFlesh] = new BossReward
-            {
-                ItemID = ItemID.WormholePotion,
-                Amount = 3,
-                Message = "You received 3 Wormhole Potions for defeating the Wall of Flesh!"
-            };
-
-            return defaultConfig;
+            // Logic to generate the URL based on your needs
+            return "https://your-terraria-server-url";
         }
-
-        private void OnNpcKilled(NpcKilledEventArgs args)
-        {
-            var player = TShock.Players[args.npc.lastInteraction];
-
-            if (player == null || !player.Active)
-                return;
-
-            int npcType = args.npc.type;
-
-            if (bossRewards.ContainsKey(npcType))
-            {
-                BossReward reward = bossRewards[npcType];
-                player.GiveItem(reward.ItemID, reward.Amount);
-                player.SendSuccessMessage(reward.Message);
-            }
-        }
-    }
-
-    public class BossReward
-    {
-        public int ItemID { get; set; }
-        public int Amount { get; set; }
-        public string Message { get; set; }
     }
 }
