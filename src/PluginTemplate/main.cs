0fd63e18a7c5d8a8feb4d42c019e527f39ef218f
That Blackwolf <107058543+aarvndhNG@users.noreply.github.com>
2023-12-20 23:29:57 +0530

    Update main.cs

--- src/PluginTemplate/main.cs
+++ src/PluginTemplate/main.cs
@@ -4,134 +4,38 @@ using Newtonsoft.Json;
 using Terraria;
 using TShockAPI;
 
-namespace MyRewardPlugin
+public class LikePlugin : TShockPlugin
 {
-    [ApiVersion(2, 1)]
-    public class MyRewardPlugin : TerrariaPlugin
+    public override void OnEnable()
     {
-        private Dictionary<int, (string name, int price)> shopItems = new Dictionary<int, (string name, int price)>();
+        PluginManager.Instance.RegisterCommand(
+            "like",
+            this,
+            typeof(LikeCommand),
+            "Give a random buff to a player",
+            "/like <player>"
+        );
 
-        public MyRewardPlugin(Main game) : base(game)
-        {
-            Order = 10;
-        }
-
-        public override void Initialize()
-        {
-            // Register commands
-            Commands.ChatCommands.Add(new Command("reward", RewardCommand, "reward", "points"));
-            Commands.ChatCommands.Add(new Command("redeem", RedeemCommand, "redeem", "points"));
-            Commands.ChatCommands.Add(new Command("shop", ShopCommand, "shop", "items"));
-
-            // Register hooks
-            NetHooks.PlayerConnect += OnPlayerConnect;
-            PlayerHooks.PlayerKilled += OnPlayerKillMob;
-
-            // Load shop items from configuration file
-            LoadShopItems();
-        }
-
-        private void LoadShopItems()
-        {
-            string configPath = Path.Combine(TShock.SavePath, "MyRewardPlugin.json");
-            if (!File.Exists(configPath))
-            {
-                Log.Info("MyRewardPlugin configuration file not found. Using default shop items.");
-                return;
-            }
-
-            try
-            {
-                shopItems = JsonConvert.DeserializeObject<Dictionary<int, (string name, int price)>>(File.ReadAllText(configPath));
-            }
-            catch (Exception ex)
-            {
-                Log.Error("Error loading MyRewardPlugin configuration: " + ex.Message);
-            }
-        }
-
-        private void RewardCommand(CommandArgs args)
-        {
-            // Check if player has enough points
-            if (TShock.Players[args.Player].GetDouble("points") < 10)
-            {
-                args.Player.SendErrorMessage("You don't have enough points to redeem that reward!");
-                return;
-            }
-
-            // Give player the reward
-            args.Player.GiveItem(12, 1); // Example reward: 1 Gold Coin
-            args.Player.SendSuccessMessage("You have redeemed your points for the reward!");
-
-            // Subtract points from player
-            TShock.Players[args.Player].SetDouble("points", TShock.Players[args.Player].GetDouble("points") - 10);
-        }
-
-        private void RedeemCommand(CommandArgs args)
-{
-    // Parse item ID from argument
-    int itemId;
-    if (!int.TryParse(args.Parameters[0], out itemId))
-    {
-        args.Player.SendErrorMessage("Invalid item ID specified!");
-        return;
+        LoadBuffsFromConfig();
     }
 
-    // Check if item exists in shop
-    if (!shopItems.ContainsKey(itemId))
+    private List<BuffData> buffs = new List<BuffData>();
+
+    private void LoadBuffsFromConfig()
     {
-        args.Player.SendErrorMessage("Item with that ID is not available for redemption!");
-        return;
-    }
-
-    // Check if player has enough points
-    if (TShock.Players[args.Player].GetDouble("points") < shopItems[itemId].price)
-    {
-        args.Player.SendErrorMessage("You don't have enough points to redeem this item!");
-        return;
-    }
-
-    // Give player the item
-    args.Player.GiveItem(itemId, 1);
-    args.Player.SendSuccessMessage("You have redeemed your points for the " + shopItems[itemId].name + "!");
-
-    // Subtract points from player
-    TShock.Players[args.Player].SetDouble("points", TShock.Players[args.Player].GetDouble("points") - shopItems[itemId].price);
-}
-
-        private void ShopCommand(CommandArgs args)
+        string configPath = Server.Instance.FilePath + "/LikeBuffs.json"; // Adjust the path as needed
+        try
         {
-            if (shopItems.Count == 0)
+            using (StreamReader reader = new StreamReader(configPath))
             {
-                args.Player.SendErrorMessage("There are no items available in the shop!");
-                return;
-            }
-
-            args.Player.SendInfoMessage("Available shop items:");
-            foreach (var item in shopItems)
-            {
-                args.Player.SendInfoMessage($" - {item.Value.name} (ID: {item.Key}) - Cost: {item.Value.price} points");
+                string json = reader.ReadToEnd();
+                List<BuffData> parsedBuffs = JsonConvert.DeserializeObject<List<BuffData>>(json); // Assuming JSON format
+                buffs.AddRange(parsedBuffs);
             }
         }
-
-        private void OnPlayerConnect(NetInfo netInfo, LegacyNetPlayer player)
+        catch (Exception ex)
         {
-            // Check if player has points stored
-            if (!TShock.Players[player].HasData("points"))
-            {
-                TShock.Players[player].SetData("points", 0); // Set initial points to 0
-            }
-        }
-
-        private void OnPlayerKillMob(object sender, PlayerKilledEventArgs e)
-        {
-            // Check if mob is a specific type
-            if (e.KilledNPC.type == NPCID.Slime)
-            {
-                // Give player points for killing the mob
-                TShock.Players[e.Player].SetDouble("points", TShock.Players[e.Player].GetDouble("points") + 1);
-                e.Player.SendSuccessMessage("You have earned 1 point for killing a Slime!");
-            }
+            Log.Write("Error loading buffs: " + ex.Message);
         }
     }
 }
