18ba00ab4c72880f9c15780f6674515ca12af1ec
That Blackwolf <107058543+aarvndhNG@users.noreply.github.com>
2024-01-21 13:18:22 +0530

    Update main.cs

--- src/PluginTemplate/main.cs
+++ src/PluginTemplate/main.cs
@@ -1,41 +1,363 @@
 using System;
-using System.IO;
-using Newtonsoft.Json;
-using Terraria;
+using System.Collections.Generic;
+using System.Linq;
 using TShockAPI;
 
-public class LikePlugin : TShockPlugin
+namespace ZombieSurvival
 {
-    public override void OnEnable()
+    [ApiVersion(2, 1)]
+    public class ZombieSurvival : TerrariaPlugin
     {
-        PluginManager.Instance.RegisterCommand(
-            "like",
-            this,
-            typeof(LikeCommand),
-            "Give a random buff to a player",
-            "/like <player>"
-        );
+        public static Dictionary<int, int> playerZombieKills = new Dictionary<int, int>();
+        public static Dictionary<int, int> playerZombieDeaths = new Dictionary<int, int>();
+        public static Dictionary<int, int> playerScore = new Dictionary<int, int>();
 
-        LoadBuffsFromConfig();
-    }
-
-    private List<BuffData> buffs = new List<BuffData>();
-
-    private void LoadBuffsFromConfig()
-    {
-        string configPath = Server.Instance.FilePath + "/LikeBuffs.json"; // Adjust the path as needed
-        try
+        public override void Initialize()
         {
-            using (StreamReader reader = new StreamReader(configPath))
+            ServerApi.Hooks.GameInitialize.Register(this, OnGameInitialize);
+            ServerApi.Hooks.PlayerJoin.Register(this, OnPlayerJoin);
+            ServerApi.Hooks.PlayerLeave.Register(this, OnPlayerLeave);
+            ServerApi.Hooks.NpcKilled.Register(this, OnNpcKilled);
+            ServerApi.Hooks.Command.Register(this, "ZombieKills", OnZombieKillsCommand);
+            ServerApi.Hooks.Command.Register(this, "ZombieDeaths", OnZombieDeathsCommand);
+            ServerApi.Hooks.Command.Register(this, "PlayerScore", OnPlayerScoreCommand);
+            ServerApi.Hooks.Command.Register(this, "StartZombieSurvival", OnStartZombieSurvivalCommand);
+        }
+
+        private void OnGameInitialize(EventArgs args)
+        {
+            // Initialize any game-wide data here
+        }
+
+        private void OnPlayerJoin(TShockAPI.Hooks.PlayerJoinEventArgs args)
+        {
+            // Initialize player-specific data here
+            playerZombieKills[args.Player.Index] = 0;
+            playerZombieDeaths[args.Player.Index] = 0;
+            playerScore[args.Player.Index] = 0;
+        }
+
+        private void OnPlayerLeave(TShockAPI.Hooks.PlayerLeaveEventArgs args)
+        {
+            // Clean up player-specific data here
+            playerZombieKills.Remove(args.Player.Index);
+            playerZombieDeaths.Remove(args.Player.Index);
+            playerScore.Remove(args.Player.Index);
+        }
+
+        private void OnNpcKilled(NpcKilledEventArgs args)
+        {
+            if (args.NPC.type == NPCID.Zombie)
             {
-                string json = reader.ReadToEnd();
-                List<BuffData> parsedBuffs = JsonConvert.DeserializeObject<List<BuffData>>(json); // Assuming JSON format
-                buffs.AddRange(parsedBuffs);
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 10;
+            }
+            else if (args.NPC.type == NPCID.ZombieEskimo)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 20;
+            }
+            else if (args.NPC.type == NPCID.ZombieViking)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 30;
+            }
+            else if (args.NPC.type == NPCID.ZombieWerewolf)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 40;
+            }
+            else if (args.NPC.type == NPCID.ZombieBee)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 50;
+            }
+            else if (args.NPC.type == NPCID.ZombieBuffalo)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 100;
+            }
+            else if (args.NPC.type == NPCID.ZombieMerman)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 150;
+            }
+            else if (args.NPC.type == NPCID.ZombieMerchant)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 200;
+            }
+            else if (args.NPC.type == NPCID.ZombieChef)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 250;
+            }
+            else if (args.NPC.type == NPCID.ZombieDoctor)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 300;
+            }
+            else if (args.NPC.type == NPCID.ZombieFisherman)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 350;
+            }
+            else if (args.NPC.type == NPCID.ZombieGoblin)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 400;
+            }
+            else if (args.NPC.type == NPCID.ZombieGiant)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 500;
+            }
+            else if (args.NPC.type == NPCID.ZombieGunner)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 600;
+            }
+            else if (args.NPC.type == NPCID.ZombieMartian)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 700;
+            }
+            else if (args.NPC.type == NPCID.ZombieNinja)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 800;
+            }
+            else if (args.NPC.type == NPCID.ZombiePirate)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 900;
+            }
+            else if (args.NPC.type == NPCID.ZombieSanta)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 1000;
+            }
+            else if (args.NPC.type == NPCID.ZombieSkeleton)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 1100;
+            }
+            else if (args.NPC.type == NPCID.ZombieWitch)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 1200;
+            }
+            else if (args.NPC.type == NPCID.ZombieWizard)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 1300;
+            }
+            else if (args.NPC.type == NPCID.ZombieWyvern)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 1400;
+            }
+            else if (args.NPC.type == NPCID.ZombieYeti)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 1500;
+            }
+            else if (args.NPC.type == NPCID.ZombieBoss)
+            {
+                int playerIndex = args.WhoKilled.Index;
+                playerZombieKills[playerIndex]++;
+                playerScore[playerIndex] += 2000;
             }
         }
-        catch (Exception ex)
+
+        private void OnZombieKillsCommand(CommandArgs args)
         {
-            Log.Write("Error loading buffs: " + ex.Message);
+            if (!args.Player.HasPermission("zombiesurvival.command.zombiekills"))
+            {
+                args.Player.SendErrorMessage("You do not have permission to use this command.");
+                return;
+            }
+
+            if (args.Parameters.Length < 1)
+            {
+                args.Player.SendErrorMessage("Usage: /zombiekills [player]");
+                return;
+            }
+
+            TShockAPI.TSPlayer targetPlayer = TShock.Players.FindPlayer(args.Parameters[0]);
+            if (targetPlayer == null)
+            {
+                args.Player.SendErrorMessage("Player not found.");
+                return;
+            }
+
+            int kills = playerZombieKills[targetPlayer.Index];
+            args.Player.SendMessage("{0} has killed {1} zombies.", targetPlayer.Name, kills);
+        }
+
+        private void OnZombieDeathsCommand(CommandArgs args)
+        {
+            if (!args.Player.HasPermission("zombiesurvival.command.zombiedeaths"))
+            {
+                args.Player.SendErrorMessage("You do not have permission to use this command.");
+                return;
+            }
+
+            if (args.Parameters.Length < 1)
+            {
+                args.Player.SendErrorMessage("Usage: /zombiedeaths [player]");
+                return;
+            }
+
+            TShockAPI.TSPlayer targetPlayer = TShock.Players.FindPlayer(args.Parameters[0]);
+            if (targetPlayer == null)
+            {
+                args.Player.SendErrorMessage("Player not found.");
+                return;
+            }
+
+            int deaths = playerZombieDeaths[targetPlayer.Index];
+            args.Player.SendMessage("{0} has died {1} times to zombies.", targetPlayer.Name, deaths);
+        }
+
+        private void OnPlayerScoreCommand(CommandArgs args)
+        {
+            if (!args.Player.HasPermission("zombiesurvival.command.playerscore"))
+            {
+                args.Player.SendErrorMessage("You do not have permission to use this command.");
+                return;
+            }
+
+            if (args.Parameters.Length < 1)
+            {
+                args.Player.SendErrorMessage("Usage: /playerscore [player]");
+                return;
+            }
+
+            TShockAPI.TSPlayer targetPlayer = TShock.Players.FindPlayer(args.Parameters[0]);
+            if (targetPlayer == null)
+            {
+                args.Player.SendErrorMessage("Player not found.");
+                return;
+            }
+
+            int score = playerScore[targetPlayer.Index];
+            args.Player.SendMessage("{0}'s score is {1}.", targetPlayer.Name, score);
+        }
+
+        private void OnStartZombieSurvivalCommand(CommandArgs args)
+        {
+           ```vbnet
+            if (!args.Player.HasPermission("zombiesurvival.command.startzombiesurvival"))
+            {
+                args.Player.SendErrorMessage("You do not have permission to use this command.");
+                return;
+            }
+
+            if (args.Parameters.Length < 1)
+            {
+                args.Player.SendErrorMessage("Usage: /startzombiesurvival [duration]");
+                return;
+            }
+
+            int duration;
+            if (!int.TryParse(args.Parameters[0], out duration))
+            {
+                args.Player.SendErrorMessage("Invalid duration.");
+                return;
+            }
+
+            TShock.Players.Broadcast("Zombie survival minigame starting in 10 seconds!", Color.Yellow);
+            TShock.Utils.Broadcast(string.Format("Zombie survival minigame will last for {0} minutes.", duration), Color.Yellow);
+            TShock.Utils.Broadcast("Kill as many zombies as you can to earn points!", Color.Yellow);
+            TShock.Utils.Broadcast("The player with the most points at the end of the minigame will be the winner!", Color.Yellow);
+
+            TShock.Scheduler.Schedule(TimeSpan.FromSeconds(10), () =>
+            {
+                TShock.Players.Broadcast("Zombie survival minigame has started!", Color.Yellow);
+                TShock.Scheduler.Schedule(TimeSpan.FromMinutes(duration), () =>
+                {
+                    TShock.Players.Broadcast("Zombie survival minigame has ended!", Color.Yellow);
+
+                    int maxScore = playerScore.Values.Max();
+                    List<KeyValuePair<int, int>> winners = playerScore.Where(kvp => kvp.Value == maxScore).ToList();
+
+                    if (winners.Count == 1)
+                    {
+                        TShockAPI.TSPlayer winner = TShock.Players[winners[0].Key];
+                        TShock.Players.Broadcast(string.Format("{0} is the winner with a score of {1}!", winner.Name, maxScore), Color.Yellow);
+                    }
+                    else
+                    {
+                        string winnersList = string.Join(", ", winners.Select(kvp => TShock.Players[kvp.Key].Name));
+                        TShock.Players.Broadcast(string.Format("{0} are the winners with a score of {1}!", winnersList, maxScore), Color.Yellow);
+                    }
+
+                    playerZombieKills.Clear();
+                ```vbnet
+            if (!args.Player.HasPermission("zombiesurvival.command.startzombiesurvival"))
+            {
+                args.Player.SendErrorMessage("You do not have permission to use this command.");
+                return;
+            }
+
+            if (args.Parameters.Length < 1)
+            {
+                args.Player.SendErrorMessage("Usage: /startzombiesurvival [duration]");
+                return;
+            }
+
+            int duration;
+            if (!int.TryParse(args.Parameters[0], out duration))
+            {
+                args.Player.SendErrorMessage("Invalid duration.");
+                return;
+            }
+
+            TShock.Players.Broadcast("Zombie survival minigame starting in 10 seconds!", Color.Yellow);
+            TShock.Utils.Broadcast("Zombie survival minigame starting in 10 seconds!", 10);
+            TShock.Utils.Broadcast("Zombie survival minigame starting in 5 seconds!", 5);
+            TShock.Utils.Broadcast("Zombie survival minigame starting in 3...2...1...GO!", 3);
+
+            TShock.Scheduler.Schedule(TimeSpan.FromSeconds(duration), () =>
+            {
+                TShock.Players.Broadcast("Zombie survival minigame has ended!", Color.Yellow);
+                TShock.Utils.Broadcast("Zombie survival minigame has ended!", 10);
+
+                int maxScore = playerScore.Values.Max();
+                int winnerIndex = playerScore.FirstOrDefault(p => p.Value == maxScore).Key;
+                TShockAPI.TSPlayer winner = TShock.Players[winnerIndex];
+
+                TShock.Players.Broadcast("{0} is the winner with a score of {1}!", winner.Name, maxScore);
+                TShock.Utils.Broadcast("{0} is the winner with a score of {1}!", winner.Name, maxScore, 10);
+
+                playerZombieKills.Clear();
+                playerZombieDeaths.Clear();
+                playerScore.Clear();
+            });
         }
     }
 }
