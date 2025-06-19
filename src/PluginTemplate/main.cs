f109a367f7e6d8fecece410f4f69dbc00618f2b1
That Blackwolf <107058543+aarvndhNG@users.noreply.github.com>
2025-06-19 20:10:18 +0530

    Update main.cs

--- src/PluginTemplate/main.cs
+++ src/PluginTemplate/main.cs
@@ -1,115 +1,92 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
+using TShockAPI;
 using Terraria;
 using TerrariaApi.Server;
-using TShockAPI;
-using Newtonsoft.Json;
 
-namespace SpecialEventsPlugin
+namespace RandomRewardPlugin
 {
     [ApiVersion(2, 1)]
-    public class SpecialEventsPlugin : TerrariaPlugin
+    public class RandomRewardPlugin : TerrariaPlugin
     {
-        public override string Name => "SpecialEventsPlugin";
-        public override string Author => "Same/AI";
-        public override string Description => "Custom welcome/leave msg and special event access by player/group.";
-        public override Version Version => new Version(1, 0, 0);
+        public override string Name => "RandomRewardPlugin";
+        public override string Author => "YourName";
+        public override string Description => "A TShock plugin that allows owners to give a random item reward to a player.";
+        public override Version Version => new Version(1, 0, 0, 0);
 
-        private static string ConfigPath => Path.Combine(TShock.SavePath, "SpecialEventsConfig.json");
-        private SpecialEventsConfig Config;
+        private readonly Random _random = new Random();
+        private readonly List<(int itemId, int minStack, int maxStack)> _rewardItems = new List<(int, int, int)>
+        {
+            (19, 1, 10),   // Iron Bar
+            (20, 1, 10),   // Copper Bar
+            (21, 1, 10),   // Silver Bar
+            (22, 1, 10),   // Gold Bar
+            (117, 10, 50), // Heart Crystal
+            (29, 5, 20),   // Life Crystal
+            (75, 10, 30)   // Mana Star
+        };
 
-        public SpecialEventsPlugin(Main game) : base(game) { }
+        public RandomRewardPlugin(Main game) : base(game)
+        {
+        }
 
         public override void Initialize()
         {
-            LoadConfig();
-            ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
-            ServerApi.Hooks.ServerLeave.Register(this, OnLeave);
-            Commands.ChatCommands.Add(new Command(CmdEvent, "event"));
+            Commands.ChatCommands.Add(new Command("randomreward.give", RewardCommand, "reward")
+            {
+                HelpText = "Gives a random item reward to a player. Usage: /reward <player>",
+                Permissions = new List<string> { "randomreward.give" }
+            });
+        }
+
+        private void RewardCommand(CommandArgs args)
+        {
+            if (!args.Player.HasPermission("randomreward.give"))
+            {
+                args.Player.SendErrorMessage("You do not have permission to use this command.");
+                return;
+            }
+
+            if (args.Parameters.Count != 1)
+            {
+                args.Player.SendErrorMessage("Usage: /reward <player>");
+                return;
+            }
+
+            string targetName = args.Parameters[0];
+            TSPlayer target = TShock.Players.FindPlayer(targetName);
+
+            if (target == null)
+            {
+                args.Player.SendErrorMessage("Player not found or not online.");
+                return;
+            }
+
+            if (!target.IsLoggedIn)
+            {
+                args.Player.SendErrorMessage("Target player must be logged in.");
+                return;
+            }
+
+            // Select a random item from the reward pool
+            var reward = _rewardItems[_random.Next(_rewardItems.Count)];
+            int stackSize = _random.Next(reward.minStack, reward.maxStack + 1);
+
+            // Give the item to the player
+            target.GiveItem(reward.itemId, stackSize);
+
+            // Notify both players
+            target.SendSuccessMessage($"You received {stackSize} x {Item.GetItemName(reward.itemId)} as a reward!");
+            args.Player.SendSuccessMessage($"You gave {target.Name} {stackSize} x {Item.GetItemName(reward.itemId)}.");
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                ServerApi.Hooks.ServerJoin.Deregister(this, OnJoin);
-                ServerApi.Hooks.ServerLeave.Deregister(this, OnLeave);
+                // No hooks to deregister
             }
             base.Dispose(disposing);
-        }
-
-        private void LoadConfig()
-        {
-            if (!File.Exists(ConfigPath))
-            {
-                Config = SpecialEventsConfig.Default();
-                File.WriteAllText(ConfigPath, JsonConvert.SerializeObject(Config, Formatting.Indented));
-            }
-            else
-            {
-                Config = JsonConvert.DeserializeObject<SpecialEventsConfig>(File.ReadAllText(ConfigPath));
-            }
-        }
-
-        private void OnJoin(JoinEventArgs args)
-        {
-            var plr = TShock.Players[args.Who];
-            if (plr != null && plr.Active)
-            {
-                string msg = Config.WelcomeMessage.Replace("{player}", plr.Name);
-                plr.SendMessage(msg, TSColor.Green);
-            }
-        }
-
-        private void OnLeave(LeaveEventArgs args)
-        {
-            var plr = TShock.Players[args.Who];
-            if (plr != null)
-            {
-                string msg = Config.LeaveMessage.Replace("{player}", plr.Name);
-                TShock.Utils.Broadcast(msg, TSColor.Red);
-            }
-        }
-
-        private void CmdEvent(CommandArgs args)
-        {
-            var plr = args.Player;
-            if (IsAllowed(plr))
-            {
-                plr.SendSuccessMessage("Welcome to the special event!");
-                // Place event code here
-            }
-            else
-            {
-                plr.SendErrorMessage("You are not allowed to join this event.");
-            }
-        }
-
-        private bool IsAllowed(TSPlayer plr)
-        {
-            if (plr == null) return false;
-            if (Config.AllowedPlayerNames.Contains(plr.Name))
-                return true;
-            foreach (var grp in Config.AllowedGroups)
-            {
-                if (plr.Group.Name.Equals(grp, StringComparison.OrdinalIgnoreCase))
-                    return true;
-            }
-            return false;
-        }
-    }
-
-    public class SpecialEventsConfig
-    {
-        public string WelcomeMessage { get; set; } = "Welcome to the server, {player}!";
-        public string LeaveMessage { get; set; } = "{player} has left. See you next time!";
-        public List<string> AllowedPlayerNames { get; set; } = new List<string> { "YourNameHere" };
-        public List<string> AllowedGroups { get; set; } = new List<string> { "admin", "eventer" };
-
-        public static SpecialEventsConfig Default()
-        {
-            return new SpecialEventsConfig();
         }
     }
 }
